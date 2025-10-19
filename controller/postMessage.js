c537dd2b3afe31efa0545e8a9d7fe53049ea0c41
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-10-19 21:26:37 +0800

    fix: duplicate post fixed by making a redirect before posting data

--- controller/postMessage.js
+++ controller/postMessage.js
@@ -2,12 +2,13 @@ const { getDatabase } = require("../model/db.js");
 
 async function postMessage(req, res) {
   const { message, user } = req.body;
+  res.redirect("/");
+
   getDatabase().push({
     text: message,
     user: user,
     added: new Date().toDateString(),
   });
-  res.redirect("/");
 }
 
 module.exports = { postMessage };
