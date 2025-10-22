a90800bba50485c58b2170912c9288e91664bbb6
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-10-22 15:17:21 +0800

    fix: change the validator minimum req for username and message field

--- middlewares/validators.js
+++ middlewares/validators.js
@@ -1,19 +1,19 @@
 const { body } = require("express-validator");
 
 const alphaErr = "must only contain letters.";
-const usernamelengthErr = "must be between 1 and 10 characters";
+const usernamelengthErr = "must be contain atleast 1 character";
 
 const validateUser = [
   body("username")
     .trim()
     .isAlpha()
     .withMessage(`Username ${alphaErr}`)
-    .isLength({ min: 1, max: 10 })
+    .isLength({ min: 1 })
     .withMessage(`Username ${usernamelengthErr}`),
   body("message")
     .trim()
-    .isLength({ min: 15 })
-    .withMessage(`Message must contain atleast 15 characters`),
+    .isLength({ min: 10 })
+    .withMessage(`Message must contain atleast 10 characters`),
 ];
 
 module.exports = validateUser;
